85cf7e733053142805dd302f77e88b1f57508844
Gozzissimo <fantini.nico@gmail.com>
2021-11-25 09:07:41 +0100

    final

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,42 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// pulsante
+generate.addEventListener('click', function () {
+    
+    // rimozione colori
+    const result = document.getElementById('result')
+    result.classList.remove('green');
+    result.classList.remove('red');
+
+
+    // calcolatore dadi
+    const userNumber = Math.floor(Math.random() * 6) + 1;
+    // console.log(userNumber);
+    const humanDice = document.getElementById('human')
+    humanDice.innerHTML = userNumber
+    
+    const pcNumber = Math.floor(Math.random() * 6) + 1;
+    // console.log(pcNumber);
+    const machineDice = document.getElementById('machine')
+    machineDice.innerHTML = pcNumber
+
+    
+    // condizioni
+    if (userNumber > pcNumber) {
+        // // console.log('La razza umana ha prevalso');
+        result.innerHTML = 'La razza umana ha prevalso'
+        result.classList.add('green');
+        
+    } else if (userNumber < pcNumber) {
+        // console.log('Le macchine hanno preso il controllo');
+        result.innerHTML = 'Le macchine hanno preso il controllo'
+        result.classList.add('red');
+    } else {
+        // console.log('Non ci sono né vincitori, né vinti...');
+        result.innerHTML = 'Non ci sono né vincitori, né vinti'
+    }
+
+});
--- mail/js/script.js
+++ mail/js/script.js
@@ -2,3 +2,38 @@
 // Chiedi all’utente la sua email,
 //     controlla che sia nella lista di chi può accedere,
 //         stampa un messaggio appropriato sull’esito del controllo.
+
+
+// pulsante
+generate.addEventListener ('click', function() {
+    
+    // array
+    const userEmails = ['paolo.rossi@gmail.com', 'marco.bianchi@yahoo.com', 'giacomo.verdi@tin.it'];
+
+    // email inserita
+    const userEmail = document.getElementById('email').value;
+    
+    // Boolean
+    let find = false; 
+    
+    // ciclo
+    for (let i = 0; i < userEmails.length; i++) {
+        
+        const element = userEmails[i];
+        
+        if (userEmail == element) {
+            find = true;
+        }
+    }
+    
+    const answer = document.createElement('div');
+    const container = document.querySelector('.container');
+
+    if (find == true) {
+        answer.innerHTML = ('La tua email è corretta!');
+    } else {
+        answer.innerHTML = ('Mi dispiace, la tua email non è nella lista');
+    }
+
+    container.append(answer);
+});
